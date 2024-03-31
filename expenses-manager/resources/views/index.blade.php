f859fa96a894fea654cd9db5161ad91f0370dd62
arickor <aricor10945@gmail.com>
2024-03-30 20:22:37 -0700

    Centered the index page

--- expenses-manager/resources/views/index.blade.php
+++ expenses-manager/resources/views/index.blade.php
@@ -7,9 +7,9 @@
         </div>
     @endif
 
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="pull-right">
+    <div style="display: flex; justify-content: center;">
+        <div style="width: 80%;">
+            <div class="float-left">
                 <a class="btn btn-success btn-sm" href="{{ route('import') }}" class="btn btn-primary">Import CSV</a>
                 <a class="btn btn-success btn-sm" href="{{ route('transaction.create') }}">
                     Create New
@@ -18,29 +18,30 @@
                     Report
                 </a>
             </div>
+            <table class="table table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Date</th>
+                        <th>Shop Name</th>
+                        <th>Money Spent</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($transactions as $item)
+                        <tr>
+                            <td>{{ $item->date }}</td>
+                            <td>{{ $item->ShopName }}</td>
+                            <td>{{ $item->MoneySpent }}</td>
+                            <td>
+                                <a class="btn btn-primary btn-sm" href="{{ route('transaction.edit', $item->id) }}">Edit</a>
+                                <a class="btn btn-danger btn-sm"
+                                    href="{{ route('transaction.destroy', $item->id) }}">Del</a>
+                            </td>
+
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
-    <table class="table table-striped">
-        <thead class="thead-dark">
-            <tr>
-                <th>Date</th>
-                <th>Shop Name</th>
-                <th>Money Spent</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($transactions as $item)
-                <tr>
-                    <td>{{ $item->date }}</td>
-                    <td>{{ $item->ShopName }}</td>
-                    <td>{{ $item->MoneySpent }}</td>
-                    <td>           
-                        <a class="btn btn-primary btn-sm" href="{{ route('transaction.edit',$item->id) }}">Edit</a>
-                        <a class="btn btn-danger btn-sm" href="{{ route('transaction.destroy',$item->id) }}">Del</a>
-                    </td>
-                    
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
 @endsection
