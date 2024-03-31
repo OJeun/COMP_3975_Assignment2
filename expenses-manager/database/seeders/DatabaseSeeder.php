5164d677b78d43a8eaea23df5b08680608d7d3cf
arickor <aricor10945@gmail.com>
2024-03-30 23:44:19 -0700

    Added an admin user

--- expenses-manager/database/seeders/DatabaseSeeder.php
+++ expenses-manager/database/seeders/DatabaseSeeder.php
@@ -5,19 +5,20 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'email' => 'aa@aa.aa',
+            'password' => Hash::make('password'),
+            'isApproved' => true,
+            'isAdmin' => true,
         ]);
     }
 }
